b73db993627057e359b00af41586cfd83df628cc
DANIELKAKAI <danielkakai3@gmail.com>
2021-03-31 00:51:07 +0300

    updated cidr and netmask methods

--- cidr_convert_api/python/convert.py
+++ cidr_convert_api/python/convert.py
@@ -1,11 +1,30 @@
 # These functions need to be implemented
+from ipaddress import IPv4Network,NetmaskValueError
+import socket
+import struct
+
+
 class CidrMaskConvert:
 
     def cidr_to_mask(self, val):
-        return val
+        host_bits = 32 - int(val)
+        try:
+            netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
+        except:
+            return 'Invalid'
+        if netmask == '0.0.0.0':
+            return 'Invalid'
+        return str(netmask)
 
     def mask_to_cidr(self, val):
-        return val
+        try:
+            cidr = IPv4Network((0, val)).prefixlen
+        except NetmaskValueError:
+            return 'Invalid'
+        if cidr == 0:
+            return 'Invalid'
+        return str(cidr)
+
 
 class IpValidate:
 
